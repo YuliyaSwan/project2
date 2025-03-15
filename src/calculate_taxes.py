4305584c5688a47782d9f75cb65a418ce5233d23
Krivova Yuliya <charrly13swan@gmail.com>
2025-03-15 21:24:52 +0300

    created func taxes and tests

--- src/calculate_taxes.py
+++ src/calculate_taxes.py
@@ -2,13 +2,13 @@ def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
     """Функция вычисляет стоимость товаров с учётом налога."""
 
     if tax_rate < 0:
-        raise ValueError('Неверный налоговый процент')
+        raise ValueError("Неверный налоговый процент")
 
     taxed_prices = []
 
     for price in prices:
         if price <= 0:
-            raise ValueError('Неверная цена')
+            raise ValueError("Неверная цена")
         tax = price * tax_rate / 100
         taxed_prices.append(price + tax)
 
--- tests/test_calculate_taxes.py
+++ tests/test_calculate_taxes.py
@@ -10,11 +10,10 @@ def prices():
     return [100, 200, 300]
 
 
-@pytest.mark.parametrize("tax_rate, expected", [(10, [110, 220, 330]),
-                                                (15, [115, 230, 345]),
-                                                (20, [120, 240, 360])
-                                                ]
-                         )
+@pytest.mark.parametrize(
+    "tax_rate, expected",
+    [(10, [110, 220, 330]), (15, [115, 230, 345]), (20, [120, 240, 360])],
+)
 def test_calculate_taxes(prices, tax_rate, expected):
     assert calculate_taxes(prices, tax_rate) == expected
 
